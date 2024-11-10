90b9aaa6920cfbc9685d9bcde02d727063a73236
oviekshagya <oviekshgy@gmail.com>
2024-11-10 18:08:34 +0700

    Add

--- app/Database/Migrations/2024-11-10-103632_Menuuser.php
+++ app/Database/Migrations/2024-11-10-103632_Menuuser.php
@@ -9,6 +9,44 @@ class Menuuser extends Migration
     public function up()
     {
         //
+        $this->forge->addField([
+            'id'           => [
+                 'type'           => 'INT',
+                 'constraint'     => 11,
+                 'unsigned'       => TRUE,
+                 'auto_increment' => TRUE
+              ],
+              'idUser'       => [
+                  'type'           => 'INT',
+                  'constraint'     => '11',
+              ],
+              'namaMenu'     => [
+                   'type'           => 'VARCHAR',
+                   'constraint'     => '255',
+              ],
+              'url'       => [
+                  'type'           => 'VARCHAR',
+                  'constraint'     => '255',
+              ],
+              'icon'       => [
+                  'type'           => 'VARCHAR',
+                  'constraint'     => '11',
+              ],
+              'idHeaderMenu'       => [
+                  'type'           => 'INT',
+                  'constraint'     => '11',
+              ],
+              'created_at'       => [
+                  'type'           => 'datetime',
+                  'constraint'     => '0',
+              ],
+              'updated_at'       => [
+                  'type'           => 'datetime',
+                  'constraint'     => '0',
+              ],
+        ]);
+        $this->forge->addKey('id', TRUE);
+        $this->forge->createTable('menuusers');
     }
 
     public function down()
